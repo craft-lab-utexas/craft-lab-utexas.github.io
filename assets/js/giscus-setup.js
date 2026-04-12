f68c77b47650f7ef9ae48110db6ddf80125e0a2c
joestubbs <joestubbs@users.noreply.github.com>
2026-04-12 20:21:57 +0000

    Deploying to gh-pages from @ craft-lab-utexas/craft-lab-utexas.github.io@de761ef72ccaf79aa600c68d8aeace395961bf48 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "craft-lab-utexas/craft-lab-utexas",
+    "data-repo": "craft-lab-utexas/craft-lab-utexas.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
